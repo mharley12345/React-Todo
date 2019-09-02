e4d2e574436c8fadb699b43bf89301e32c0b64c8
Mike Harley <mharley12345@gmail.com>
2019-09-02 10:06:55 -0700

    Updates placeholder message in TodoForm

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,7 +3,7 @@ import React from 'react'
 const TodoForm = props => {
     return(
         <form>
-        <input placeholder="ToDo ......."></input>
+        <input placeholder="Add A ToDo ......."></input>
         <button>&#9989;</button>
         <button>&#10060;</button>
 
