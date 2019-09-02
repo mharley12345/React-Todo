a3a1a4ece29f8f5901ac76411d7f546da5d7cdfa
Mike Harley <mharley12345@gmail.com>
2019-09-02 11:09:09 -0700

    Adds event handlers to TodoForm.js

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,9 +3,9 @@ import React from 'react'
 const TodoForm = props => {
     return(
         <form>
-        <input placeholder="Add A ToDo ......."></input>
-        <button>&#9989;</button>
-        <button>&#10060;</button>
+        <input onChange={props.handleChange} placeholder="Add A ToDo ......."></input>
+        <button onClick={props.addTodo}><span aria-labelledby="jsx-ally/accessible-emoji" role="img">&#9989;</span></button>
+        <button onClick={props.deleteTodo}><span aria-labelledby="jsx-ally/accessible-emoji" role="img">&#10060;</span></button>
 
         </form>
     )
